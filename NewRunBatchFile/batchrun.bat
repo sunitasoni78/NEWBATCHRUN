ce8855ba89ac50ef86e654c8bc12a6673135d0c7
sunita <2011sunitasoni@gmail.com>
2021-07-10 16:58:46 +0530

    batch changed

--- NewRunBatchFile/batchrun.bat
+++ NewRunBatchFile/batchrun.bat
@@ -1,4 +1,4 @@
-set projectLocation=E:\JOB\eclipsworkspace\NewRunBatchFile
+java set projectLocation=E:\JOB\eclipsworkspace\NewRunBatchFile
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*;
 java org.testng.TestNG %projectLocation%\testng.xml
